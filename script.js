78eec4ebf7bd221ad7cd96b2546236d1ccef77bf
DianChen-Korff <79322059+DianChen-Korff@users.noreply.github.com>
2021-05-20 16:04:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+
 console.log("Hello Winc Academy");
+
+// grote som
+
+console.log(1230941 * 1823794);
+console.log(637263 / 54);
+
+/* blok comment
+console.log(1230941 * 1823794);
+console.log(637263 / 54);
+*/
+
+// Code die niet "aan" staat zie je niet in de console. 
